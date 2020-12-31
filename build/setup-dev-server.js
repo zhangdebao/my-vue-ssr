2e5507d26fa780f60ace5e561460e209e927d0ac
张德保 <zhangdb@fosun.com>
2020-12-31 16:12:11 +0800

    feat(): 热更新

--- build/setup-dev-server.js
+++ build/setup-dev-server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const chokidar = require('chokidar')
 const webpack = require('webpack')
 const devMiddleware = require('webpack-dev-middleware') // 文件存储在内存中
+const hotMiddleWare = require('webpack-hot-middleware')
 const resolve = file => path.resolve(__dirname, file)
 module.exports = (server, callback) => { 
   let ready
@@ -54,6 +55,12 @@ module.exports = (server, callback) => {
 
   // 监视构建 clientManifest -> 调用update -> 调用Renderer渲染器
   const clientConfig = require('./webpack.client.config')
+  clientConfig.plugins.push(new webpack.HotModuleReplacementPlugin())
+  clientConfig.entry.app = [
+    'webpack-hot-middleware/client', // 和服务器交互处理热更新一个客户端脚本
+    clientConfig.entry.app
+  ]
+  clientConfig.output.filename = '[name].js' // 热更新模式下确保一致的hash
   const clientCompiler = webpack(clientConfig)
   const clientDevMiddleware = devMiddleware(clientCompiler, { // 会自动的执行打包构建， 默认也是按照监视的方式执行打包构建, 所以下面就不用手动调用clientCompiler.watch(了
     publicPath: clientConfig.output.publicPath,
@@ -67,7 +74,9 @@ module.exports = (server, callback) => {
     )
     update()
   })
-
+  server.use(hotMiddleWare(clientCompiler, {
+    log: false // 关闭它本身的日志输出 
+  }))
   // 重要！！！ 将clientDevMiddleware挂载到Express服务中，提供对其内部内存中数据的访问
   server.use(clientDevMiddleware)
   return onReady
